94ba550062a9c138dd92fee373830dd3ad316487
Mason <davismasont@gmail.com>
2022-10-28 16:47:24 -0700

    add read me file and content

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
 # note-taker
+
+
+## Deployed link
+* [note taking App](https://sivad-note-taker.herokuapp.com/)
+
+## site picture
+![site](note-taker.png)
+
+
+
+## Technologies Used
+- HTML : Used to format the content of the page
+- CSS : Used to style the HTML on the web browser
+- JavaScript : Used to give the page logical functions 
+- javascript (express) : module used to run servers in a js file
+- heroku : to deply the website and server to the web
+- Git : Used to version control the code during the creation process
+- GitHub : hosting the repositroy responsible for the website
+
+## Authors
+- writen styled and built by Mason Davis
+
+## summary
+This repository contains all the code to generate and host a website meant to make note taking easier. it has a smooth interface and functional commands including viewing old notes, deleting notes and adding new notes.
+
+## Code Snippet
+This is a snippet Showing the formula used to display the content on the page, it is how the server gets the content that it is storing.
+
+```javaScript
+app.get('/api/notes',(req,res) =>{
+res.json(noteContent)
+});
+```
+
+
+## Author links
+[LinkedIn](https://www.linkedin.com/in/davis-mason-t/)
+[GitHub](https://github.com/Md7113)
--- server.js
+++ server.js
@@ -1,28 +1,32 @@
 const express = require('express')
 const fs = require('fs')
+//this is a node library that can be used to create a unique ID
 const uniqid = require('uniqid')
 const path = require('path')
 const app = express()
 let noteContent = require('./db/db.json')
-//add or horoku stuff
 const PORT = process.env.PORT || 3001
 
+// this section is meant to enstantiate deifferent parts of the app. allow the public file to be viewed as static and allows the server to parse json data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
+// this is for the root file path on the server, meant to keep the content meant for the main page on search
 app.get('/', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/index.html'))
 );
-
+//similar to the main page this works with the notes.html page to get the content for the note writing page
 app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/notes.html'))
 )
 
+//this is meant specifically to collect the data from the already writen notes and display it on the page
 app.get('/api/notes',(req,res) =>{
 res.json(noteContent)
 });
 
+// this is the etra step meant to remove a writen note, it is based on the function from class we used only reverse engineered to speciffically work for my purpose
 app.delete('/api/notes/:id', (req,res) =>{
     
     let deleteId = req.params.id
@@ -31,6 +35,7 @@ app.delete('/api/notes/:id', (req,res) =>{
           console.error(err);
         } else {
           const parsedNotes = JSON.parse(data);
+          //this is the main difference here it talks about where it collects the data of notes finds the one you want to delete and then splices it from the data
             for(i=0;i<parsedNotes.length;i++){
                 if(parsedNotes[i].id == deleteId){
                     parsedNotes.splice(i,1)
@@ -51,6 +56,8 @@ app.delete('/api/notes/:id', (req,res) =>{
       res.json(noteContent)
 })
 
+// this is also cresated from a function we were tought in class, lesson 11 activity 20 where we posted something to an array we pulled from another page
+// this is the inspiration for the delete function as well
 app.post('/api/notes', (req, res) => {
     const {title,text} = req.body
     if(title && text){
@@ -83,7 +90,7 @@ app.post('/api/notes', (req, res) => {
 
 
 
-
+// this is where the server listens for changes in port links
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} `)
 );
